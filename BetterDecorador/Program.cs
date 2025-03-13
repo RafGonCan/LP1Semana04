e9dcc817b186c1c32bf85ef08e5775e8a93c674a
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-13 22:59:58 +0000

    implement int arg

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*',4));
+            Console.WriteLine(Decor("Good Evening!", '.',2));
+        }
+        private static string Decor (string frase, char dec, int count)
+        {
+            return $"{dec}{dec}{dec} {frase} {dec}{dec}{dec}";
+        }
+    }
+}
