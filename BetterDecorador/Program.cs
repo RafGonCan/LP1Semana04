6664526fbc7aa43ed21c85e7b7a87bc458c5393c
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-13 23:29:59 +0000

    complete program and fixed the code

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,7 +7,18 @@ namespace BetterDecorador
     {
         private static void Main(string[] args)
         {
-            
+            if (args.Length == 3)
+            {
+                string s = args[0];
+                char dec = Convert.ToChar(args[1]);
+                int count = Convert.ToInt32(args[2]);
+
+                Console.WriteLine(Decor(s, dec, count));
+            }
+            else
+            {
+                Console.WriteLine("You are missing some args!");
+            }
         }
         private static string Decor (string s, char dec, int count)
         {
@@ -16,7 +27,7 @@ namespace BetterDecorador
 
             for (number = 0; number < count; number++)
             {
-                decors += decors;
+                decors += dec;
             }
 
             return $"{decors} {s} {decors}";
