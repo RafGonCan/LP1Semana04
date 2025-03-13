41e72bae94f743c7438b3eea0c357b03f0f3b567
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-13 23:39:32 +0000

    add comments xml

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -3,8 +3,16 @@ using System.Text;
 
 namespace BetterDecorador
 {
+    /// <summary>
+    /// Write a sentence with chars on the side separated by a space and
+    /// the user can choose the char and its quantity
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Writes a sentence given by the user and "embellishes" it with chars
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             if (args.Length == 3)
@@ -20,6 +28,13 @@ namespace BetterDecorador
                 Console.WriteLine("You are missing some args!");
             }
         }
+        /// <summary>
+        /// count the number of the chars
+        /// </summary>
+        /// <param name="s">sentence given by user</param>
+        /// <param name="dec">char given by user</param>
+        /// <param name="count">count how many times the char repeat in the sentence</param>
+        /// <returns>Sentence with the number given by user in chars</returns>
         private static string Decor (string s, char dec, int count)
         {
             string decors = "";
