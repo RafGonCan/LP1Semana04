3c74bdf2de0741b9a1f1210f391f5059cb2c0299
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-14 00:10:48 +0000

    create method GetSpecial

---
+++ Special/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            GetSpecial(0);
+        }
+        private static int GetSpecial (int n)
+        {
+            int [] numbers = new int [] {0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341};
+            
+        }
+    }
+}
